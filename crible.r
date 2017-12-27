9cc22fe7fad4ee50b722f8d0f0fc0b9836e7266f
Roxane <roxane.bellot@gmail.com>
2017-12-27 18:53:35 +0100

    other algo and benchmark in R

--- crible.r
+++ crible.r
@@ -4,7 +4,7 @@
 # prend un entier en paramètre
 # Par exemple :    cribleEratosthene(120)
 
-
+# nous
 cribleEratosthene <- function(n){
 	if (n < 2) return(NULL)
 	res <- rep(T, n)
@@ -17,3 +17,61 @@ cribleEratosthene <- function(n){
 	}
 	which(res)
 }
+
+# eux
+
+estPremier <- function(entier){
+   
+    if(entier==1){
+        return(0)
+        }
+    if(entier==2){
+        return(2)}
+    else{
+        valMaxATest=round(sqrt(entier))
+        }
+   
+    for(i in 2:valMaxATest){
+        if((entier%%i)==0){
+            return (0)
+            }
+        }
+    return (entier)
+   
+    }
+
+cribleEratosthene_eux <- function(limite){
+    entierToLimite<-1:limite
+    quiEstPremier=lapply(entierToLimite,estPremier)
+    return (quiEstPremier[quiEstPremier>0])
+
+    }
+
+# calcul
+calcul <- function(nous, params){
+	print(nous)
+	inner <- list(length(params))
+	j = 1
+	while (j <= length(params)){
+		start.time <- Sys.time()
+		print(params[j])
+		if (nous){
+			cribleEratosthene(params[j])
+		}
+		else {
+			cribleEratosthene_eux(params[j])
+		}
+		end.time <- Sys.time()
+		inner[j] = end.time - start.time
+		print(end.time - start.time)
+		j = j + 1
+	}
+	inner
+}
+
+analyse <- function(){
+	params = c(100, 1000, 10000, 100000, 1000000, 10000000)
+	list(calcul(T, params), calcul(F, params))
+}
+
+analyse()
